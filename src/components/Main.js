d71a321178881407eb4ec363dc9693f3d8af380c
Tolibjon <tolibjohn@gmail.com>
2021-09-09 12:58:29 +0500

    Add functionality to show markets

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './Header';
+import { fetchAllMarkets } from '../redux/categories/categories';
 
-const Main = () => (
-  <div>
-    <Header title="Remote" count="758" />
-    <ul className="categories">
-      <li><NavLink to="/details">Finance</NavLink></li>
-    </ul>
-  </div>
-);
+const Main = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllMarkets());
+  }, []);
+
+  const state = useSelector((state) => state.stockmarketReducer);
+  return (
+    <div>
+      <Header title="Markets" count={state.totalCap} />
+      <ul className="categories">
+        {state.markets.map((market) => (
+          <li key={market}>
+            <NavLink to="/details">
+              <span>{market}</span>
+              <div>
+                {state.list.filter((exchange) => exchange.exchangeShortName === market)
+                  .map((a) => a.marketCap).reduce((a, b) => a + b)}
+                <span>USD</span>
+              </div>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Main;
