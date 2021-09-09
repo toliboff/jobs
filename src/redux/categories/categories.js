96f93969aaaf4c4d1b13de0ebb976e332a6ce7ac
Tolibjon <tolibjohn@gmail.com>
2021-09-09 14:47:18 +0500

    Change API key

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -17,7 +17,7 @@ const calculateTotal = (arr) => intoBillion(arr.map((cap) => cap.marketCap)
   .reduce((a, b) => a + b));
 
 export const fetchAllMarkets = () => async (dispatch) => {
-  const fetchedData = await fetch('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&apikey=6c2d08df2b4645477a2cfe6926f2b36f');
+  const fetchedData = await fetch('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&apikey=7eac2e3f73d161944273338301184f8e');
   const json = await fetchedData.json();
   const market = {
     markets: getMarkets(json),
--- src/redux/companies/companies.js
+++ src/redux/companies/companies.js
@@ -5,6 +5,7 @@ const intoBillion = (amount) => (amount / 1000000000).toFixed(2);
 
 const getCompanies = (arr) => arr.map((m) => ({
   name: m.companyName,
+  symbol: m.symbol,
   marketCap: intoBillion(m.marketCap),
 }));
 
@@ -12,7 +13,7 @@ const calculateTotal = (arr) => intoBillion(arr.map((cap) => cap.marketCap)
   .reduce((a, b) => a + b));
 
 export const fetchCompanies = (payload) => async (dispatch) => {
-  const fetchedData = await fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&exchange=${payload}&apikey=6c2d08df2b4645477a2cfe6926f2b36f`);
+  const fetchedData = await fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&exchange=${payload}&apikey=7eac2e3f73d161944273338301184f8e`);
   const json = await fetchedData.json();
   const companies = {
     companies: getCompanies(json),
