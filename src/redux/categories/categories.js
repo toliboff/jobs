d7b16bfba3fd976b0ac094daa59a49d23a8ea5a8
Tolibjon <tolibjohn@gmail.com>
2021-09-09 14:02:30 +0500

    Add helper function

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,17 +1,20 @@
 const FETCH_ALL_MARKETS = 'JOBS/FETCH_ALL_MARKETS';
 const initialState = { markets: [], companies: [] };
 
+const intoBillion = (amount) => (amount / 1000000000).toFixed(2);
+
 const getMarkets = (arr) => {
   const markets = Array.from(new Set(arr.map((market) => market.exchangeShortName)));
   return markets.map((m) => ({
     name: m,
-    marketCap: arr.filter((ex) => ex.exchangeShortName === m)
+    marketCap: intoBillion(arr.filter((ex) => ex.exchangeShortName === m)
       .map((cap) => cap.marketCap)
-      .reduce((a, b) => a + b),
+      .reduce((a, b) => a + b)),
   }));
 };
 
-const calculateTotal = (arr) => arr.map((cap) => cap.marketCap).reduce((a, b) => a + b);
+const calculateTotal = (arr) => intoBillion(arr.map((cap) => cap.marketCap)
+  .reduce((a, b) => a + b));
 
 export const fetchAllMarkets = () => async (dispatch) => {
   const fetchedData = await fetch('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&apikey=6c2d08df2b4645477a2cfe6926f2b36f');
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,8 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import stockmarketReducer from './categories/categories';
+import companiesReducer from './companies/companies';
 
-const reducers = combineReducers({ stockmarketReducer });
+const reducers = combineReducers({ stockmarketReducer, companiesReducer });
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
