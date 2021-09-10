a513dec35c1c6e7c0f6cd02dfb8d895502e0243d
Tolibjon <tolibjohn@gmail.com>
2021-09-10 11:57:47 +0500

    Remove duplicated folder

--- src/redux/__mocks__/api.js
+++ src/redux/__mocks__/api.js
@@ -1,43 +1,40 @@
-export const fetchMarketData = async () => {
-  console.log('Mock API');
-  return Promise.resolve([
-    {
-      id: 1,
-      ticker: 'GOOG',
-      companyName: 'Google Inc.',
-      marketCap: 987654321445,
-      exchangeShortName: 'NASDAQ',
-    },
-    {
-      id: 2,
-      ticker: 'AAPL',
-      companyName: 'Apple Inc.',
-      marketCap: 823456789123,
-      exchangeShortName: 'NASDAQ',
-    },
-    {
-      id: 3,
-      ticker: 'AMD',
-      companyName: 'AMD Inc.',
-      marketCap: 723456789123,
-      exchangeShortName: 'TSX',
-    },
-    {
-      id: 4,
-      ticker: 'SNY',
-      companyName: 'AMD Inc.',
-      marketCap: 623456789123,
-      exchangeShortName: 'AMEX',
-    },
-    {
-      id: 5,
-      ticker: 'SNY',
-      companyName: 'AMD Inc.',
-      marketCap: 523456789123,
-      exchangeShortName: 'AMEX',
-    },
-  ]);
-};
+export const fetchMarketData = async () => Promise.resolve([
+  {
+    id: 1,
+    ticker: 'GOOG',
+    companyName: 'Google Inc.',
+    marketCap: 987654321445,
+    exchangeShortName: 'NASDAQ',
+  },
+  {
+    id: 2,
+    ticker: 'AAPL',
+    companyName: 'Apple Inc.',
+    marketCap: 823456789123,
+    exchangeShortName: 'NASDAQ',
+  },
+  {
+    id: 3,
+    ticker: 'AMD',
+    companyName: 'AMD Inc.',
+    marketCap: 723456789123,
+    exchangeShortName: 'TSX',
+  },
+  {
+    id: 4,
+    ticker: 'SNY',
+    companyName: 'AMD Inc.',
+    marketCap: 623456789123,
+    exchangeShortName: 'AMEX',
+  },
+  {
+    id: 5,
+    ticker: 'SNY',
+    companyName: 'AMD Inc.',
+    marketCap: 523456789123,
+    exchangeShortName: 'AMEX',
+  },
+]);
 
 export const fetchCompanyData = async (market) => {
   const wholeMarket = [
